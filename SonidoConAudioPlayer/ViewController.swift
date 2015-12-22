edf296f1b0aedac0ad71dff0d60f08a064c6744a
NetzerRivera <netzerrivera@gmail.com>
2015-12-21 19:54:20 -0600

    Versión 1

--- SonidoConAudioPlayer/ViewController.swift
+++ SonidoConAudioPlayer/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  SonidoConAudioPlayer
@@ -7,19 +8,56 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
 
+    private var reproductor: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    // inicializar
+        
+        let sonidoURL = NSBundle.mainBundle().URLForResource("Inspiracion", withExtension: "mp3")
+    do{
+        
+        try reproductor = AVAudioPlayer(contentsOfURL: sonidoURL!)
+        
+    }catch {
+        print("Error al cargar el archivo de sonido")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func stop() {
+        
+        if reproductor.playing{
 
+        reproductor.stop()
+            reproductor.currentTime = 0.0
+        
+        }
 
+    }
+    @IBAction func pause() {
+        if reproductor.playing{
+            reproductor.pause()
+        }
+    }
+    @IBAction func play() {
+        if !reproductor.playing{
+            reproductor.play()
+        }
+
+        
+    }
 }
 
